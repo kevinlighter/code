5322d7a2e9729cb51009ac75897628e617465c17
Kevin Chen <apple@chenkailiang.local>
2018-08-14 20:36:54 -0700

    finish_linked_list_unit_test

--- common/data_structure/list/test_linked_list.cpp
+++ common/data_structure/list/test_linked_list.cpp
@@ -291,144 +291,128 @@ BOOST_AUTO_TEST_CASE(test_alternating_spilt_func) {
 	free(node2);
 }
 	
+BOOST_AUTO_TEST_CASE(test_shuffle_merge_func)
+{
+	Node* node1 = buildOneTwoThree();
+	Node* node2 = NULL;
+	Node** ref_head = &node2;
+
+	push_front(ref_head, 4);
+	push_front(ref_head, 6);
+
+	Node* result = shuffle_merge(&node1, &node2);
+
+	BOOST_REQUIRE(val_nth(&result, 0) == 1);
+	BOOST_REQUIRE(val_nth(&result, 1) == 6);
+	BOOST_REQUIRE(val_nth(&result, 2) == 2);
+	BOOST_REQUIRE(val_nth(&result, 3) == 4);
+	BOOST_REQUIRE(val_nth(&result, 4) == 3);
+
+	free(node1);
+	free(node2);
+	free(result);
+}
+
+BOOST_AUTO_TEST_CASE(test_sorted_merge_func)
+{
+	Node* node1 = NULL;
+	Node* node2 = NULL;
+	Node** ref_head1 = &node1;
+	Node** ref_head2 = &node2;
+
+	push_front(ref_head1, 5);
+	push_front(ref_head1, 3);
+
+	push_front(ref_head2, 8);
+	push_front(ref_head2, 4);
+	push_front(ref_head2, 2);
+
+
+	Node* result = sorted_merge(ref_head1, ref_head2);
+
+	BOOST_REQUIRE(val_nth(&result, 0) == 2);
+	BOOST_REQUIRE(val_nth(&result, 1) == 3);
+	BOOST_REQUIRE(val_nth(&result, 2) == 4);
+	BOOST_REQUIRE(val_nth(&result, 3) == 5);
+	BOOST_REQUIRE(val_nth(&result, 4) == 8);
+
+	free(node1);
+	free(node2);
+	free(result);
+}
+
+BOOST_AUTO_TEST_CASE(test_merge_sort_func)
+{
+	Node* node = NULL;
+	Node** ref_head = &node;
+
+	push_front(ref_head, 5);
+	push_front(ref_head, 4);
+	push_front(ref_head, 52);
+	push_front(ref_head, 51);
+	push_front(ref_head, 15);
+
+	merge_sort(ref_head);
+
+	BOOST_REQUIRE(val_nth(ref_head, 0) == 4);
+	BOOST_REQUIRE(val_nth(ref_head, 1) == 5);
+	BOOST_REQUIRE(val_nth(ref_head, 2) == 15);
+	BOOST_REQUIRE(val_nth(ref_head, 3) == 51);
+	BOOST_REQUIRE(val_nth(ref_head, 4) == 52);
+
+	free(node);
+}
+
+BOOST_AUTO_TEST_CASE(test_sort_intersect_func)
+{
+	Node* node1 = buildOneTwoThree();
+	Node* node2 = buildOneTwoThree();
+
+	Node* result = sorted_intersect(&node1, &node2);
+
+	BOOST_REQUIRE(val_nth(&node1, 0) == 1);
+	BOOST_REQUIRE(val_nth(&node1, 1) == 2);
+	BOOST_REQUIRE(val_nth(&node1, 2) == 3);
+
+	BOOST_REQUIRE(val_nth(&node2, 0) == 1);
+	BOOST_REQUIRE(val_nth(&node2, 1) == 2);
+	BOOST_REQUIRE(val_nth(&node2, 2) == 3);
+
+	BOOST_REQUIRE(val_nth(&result, 0) == 3);
+	BOOST_REQUIRE(val_nth(&result, 1) == 2);
+	BOOST_REQUIRE(val_nth(&result, 2) == 1);
+
+	free(node1);
+	free(node2);
+	free(result);
+}
+
+BOOST_AUTO_TEST_CASE(test_reverse_func)
+{
+	Node* node = buildOneTwoThree();
+	Node** ref_head = &node;
+
+	reverse(ref_head);
+
+	BOOST_REQUIRE(val_nth(ref_head, 0) == 3);
+	BOOST_REQUIRE(val_nth(ref_head, 1) == 2);
+	BOOST_REQUIRE(val_nth(ref_head, 2) == 1);
+
+	free(node);
+}
+
+BOOST_AUTO_TEST_CASE(test_recursive_reverse_func)
+{
+	Node* node = buildOneTwoThree();
+	Node** ref_head = &node;
+
+	recursive_reverse(ref_head);
+
+	BOOST_REQUIRE(val_nth(ref_head, 0) == 3);
+	BOOST_REQUIRE(val_nth(ref_head, 1) == 2);
+	BOOST_REQUIRE(val_nth(ref_head, 2) == 1);
+
+	free(node);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
-
-
-// Node* test_shuffle_merge(Node** first_ref, Node** second_ref)
-// {
-// 	return shuffle_merge(first_ref, second_ref);
-// }
-
-// Node* test_sorted_merge(Node** first_ref, Node** second_ref)
-// {
-// 	return sorted_merge(first_ref, second_ref);
-// }
-
-// void test_sorted_intersect() {
-// 	auto head1 = create_dummy(0);
-// 	push_back(&head1, 2);
-// 	push_back(&head1, 4);
-// 	push_back(&head1, 6);
-// 	push_back(&head1, 8);
-// 	push_back(&head1, 10);
-
-// 	auto head2 = create_dummy(1);
-// 	push_back(&head2, 3);
-// 	push_back(&head2, 4);
-// 	push_back(&head2, 7);
-// 	push_back(&head2, 9);
-// 	push_back(&head2, 10);
-
-// 	std::cout << "intersect1: " << head1 << std::endl;
-// 	std::cout << "intersect2: " << head2 << std::endl;
-
-// 	auto result = sorted_intersect(&head1, &head2);
-
-// 	std::cout << "intersect 3: " << result << std::endl;
-// }
-
-// void test_reverse()
-// {
-// 	auto head = buildOneTwoThree();
-// 	reverse(&head);
-// 	std::cout << "1:" << head << std::endl;
-// }
-
-// void test_recursive_reverse()
-// {
-// 	auto head = buildOneTwoThree();
-// 	recursive_reverse(&head);
-// 	std::cout << "1:" << head << std::endl;
-// }
-
-
-// int main(int argc, char* argv[]) {
-
-// 	auto head = test_create_head();
-// 	//std::cout << head << std::endl;
-// 	test_push_front_one_to_five(&head);
-// 	print(&head);
-
-// 	test_push_back_six_to_ten(&head);
-// 	print(&head);
-
-// 	test_push_by_position(&head);
-// 	print(&head);
-
-// 	test_count_func(&head, 11);
-
-// 	test_val_nth_func(&head, 3);
-
-// 	//test_delete_list_func(&head);
-// 	test_delete_front_func(&head);
-// 	print(&head);
-
-// 	test_delete_n_func(&head, 14);
-// 	print(&head);
-
-// 	// auto node = create_dummy(0);
-// 	// auto build = buildOneTwoThree();
-// 	// print(&build);
-// 	// test_sorted_insert(&build, node);
-// 	// print(&build);
-
-// 	print(&head);
-// 	Node* headTwo = create_dummy(10);
-
-// 	test_append(&head, &headTwo);
-// 	print(&head);
-// 	// print(&headTwo);
-
-
-// 	test_insert_sort(&head);
-// 	print(&head);
-
-// 	test_remove_duplicate(&head);
-// 	print(&head);
-
-// 	std::cout << "length of list: " << length(head) << std::endl;
-
-// 	Node* front_head = NULL;
-// 	Node* back_head = NULL;
-
-// 	test_front_back_spilt(&(head), &front_head, &back_head);
-// 	print(&front_head);
-// 	print(&back_head);
-
-// 	auto dest_head = buildOneTwoThree();
-// 	auto source_head = buildOneTwoThree();
-// 	test_move_node(&dest_head, &source_head);
-// 	print(&dest_head);
-// 	print(&source_head);
-
-
-// 	auto head1 = create_dummy();
-// 	auto head2 = create_dummy();
-	
-// 	test_alternating_spilt(&head, &head1, &head2);
-// 	print(&head);
-// 	print(&head1);
-// 	print(&head2);
-
-// 	insert_sort(&head1);
-
-// 	auto first_head = buildOneTwoThree();
-// 	auto second_head = buildOneTwoThree();
-
-// 	auto new_head = test_sorted_merge(&head1, &second_head);
-// 	std::cout << "aaa: " << new_head << std::endl;
-
-// 	push_back(&new_head, 5);
-// 	merge_sort(&new_head);
-
-// 	std::cout << std::endl;
-// 	print(&new_head);
-
-// 	test_sorted_intersect();
-
-// 	test_reverse();
-
-// 	test_recursive_reverse();
-
-// }
