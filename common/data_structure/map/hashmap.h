9990931e71b7c7040515a7bc5630fc8c00af9466
kailiang chen <kevinlighter007@gmail.com>
2018-09-11 17:54:01 -0700

    tuesday

---
+++ common/data_structure/map/hashmap.h
@@ -0,0 +1,52 @@
+#ifndef _HASHMAP_H_
+#define _HASHMAP_H_
+
+#include <sstream>
+#include "stringmap.h"
+
+using namespace std;
+
+namespace common {
+namespace map {
+
+template <class KeyType, class ValueType>
+class HashMap {
+
+public:
+	HashMap();
+
+	~HashMap() = default;
+
+	/// general function for getting Value from Key type
+	ValueType get(const KeyType& key) const;
+
+	/// general function for putting new pairs into map
+	void put(const KeyType& key, const ValueType& value);
+
+private:
+	StringMap string_map_;
+
+};
+
+template <typename KeyType, typename ValueType>
+HashMap<KeyType, ValueType>::HashMap() :
+	string_map_(StringMap())
+{
+}
+
+template <typename KeyType, typename ValueType>
+ValueType HashMap<KeyType, ValueType>::get(const KeyType& key) const 
+{
+	string str = this->string_map_.get(to_string(key));
+	istringstream stream(str);
+	ValueType result;
+	stream >> result;
+	return result;
+}
+
+
+
+} // namespace map
+} // namespace common
+
+#endif
